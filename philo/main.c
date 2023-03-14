7c5a651de7ac38c9cf174794db6337a310096fe7
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-03-14 13:36:55 +0100

    numbering of philosophers works

--- philo/main.c
+++ philo/main.c
@@ -6,20 +6,20 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 11:07:19 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/03/14 11:38:53 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:33:12 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ptjoinall(t_data *d)
+void	ptjoinall(t_data **d)
 {
 	int	i;
 
 	i = 0;
-	while (i < d->nump)
+	while (i < d[0]->nump)
 	{
-		pthread_join(d->tids[i], NULL);
+		pthread_join(d[i]->tid, NULL);
 		i++;
 	}
 }
@@ -31,51 +31,66 @@ void	*philo(void *arg)
 
 	d = (t_data *) arg;
 	philnum = d->philonum;
-	printf("Runs\n ");
 	printf("Philo number: %i\n", philnum);
 
 	return (NULL);
 }
 
-int	filld(int argc, char **argv, t_data *d)
+int	filld(int argc, char **argv, t_data **d)
 {
+	int	i;
+	int	nump;
+
 	if (argc < 5)
 	{
 		printf("Please enter at least 4 arguments!\n");
 		return (1);
 	}
-	d->nump = ft_atoi(argv[1]);
-	d->ttodie = ft_atoi(argv[2]);
-	d->ttoeat = ft_atoi(argv[3]);
-	d->ttosleep = ft_atoi(argv[4]);
-	if (argc == 6)
-		d->numberofeats = ft_atoi(argv[5]);
-	d->tids = malloc(d->nump * sizeof(pthread_t));
-	if (d->tids == NULL)
-		return (1);
+	i = 0;
+	nump = ft_atoi(argv[1]);
+	while (i < nump)
+	{
+		d[i]->nump = nump;
+		d[i]->ttodie = ft_atoi(argv[2]);
+		d[i]->ttoeat = ft_atoi(argv[3]);
+		d[i]->ttosleep = ft_atoi(argv[4]);
+		d[i]->philonum = i + 1;
+		if (argc == 6)
+			d[i]->numberofeats = ft_atoi(argv[5]);
+		i++;
+	}
 	return (0);
 }
 
-void	createthreads(t_data *d)
+void	createthreads(t_data **d)
 {
 	int	i;
 
 	i = 0;
-	while (i < d->nump)
+	while (i < d[0]->nump)
 	{
-		d->philonum = i + 1;
-		pthread_create(&d->tids[i], NULL, philo, d);
+		pthread_create(&d[i]->tid, NULL, philo, d[i]);
 		i++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	t_data	d;
+	t_data	**d;
+	int		i;
+	int		nump;
 
-	if (filld(argc, argv, &d) == 1)
+	i = 0;
+	nump = ft_atoi(argv[1]);
+	d = malloc(nump * sizeof(t_data *));
+	while (i < nump)
+	{
+		d[i] = malloc(nump * sizeof(t_data));
+		i++;
+	}
+	if (filld(argc, argv, d) == 1)
 		return (0);
-	createthreads(&d);
-	ptjoinall(&d);
-	free(d.tids);
+	createthreads(d);
+	ptjoinall(d);
+	free(d); //free every node seperate
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 09:49:03 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/03/14 11:26:21 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:08:16 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ typedef struct s_data
 	int				ttosleep;
 	int				numberofeats;
 	int				philonum;
-	pthread_t		*tids; //maybe remove this
+	pthread_t		tid;
 }				t_data;
 
 int	ft_atoi(const char *nptr);
